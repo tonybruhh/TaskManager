54bfd624733e44a2b8e6d95e34e7e6a1664e92af
Anton Khlyustov <khlyustov.anton@gmail.com>
2025-08-28 20:13:58 +0300

    Added TaskItem into the migrations

--- TaskManager.Api/Infrastructure/AppDbContext.cs
+++ TaskManager.Api/Infrastructure/AppDbContext.cs
@@ -6,5 +6,18 @@ namespace TaskManager.Api.Infrastructure;
 
 public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext<AppUser>(options)
 {
-    
+    public DbSet<TaskItem> Tasks => Set<TaskItem>();
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<TaskItem>(b =>
+        {
+            b.HasOne(t => t.User)
+                .WithMany()
+                .HasForeignKey(t => t.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        });
+    }
 }
